b904e9aadf35dd24bb18cbf9676c1e513ac7303d
Zeheng Li <imzehengl@gmail.com>
2025-11-07 15:38:36 -0700

    Update streamlit_app.py

--- {{cookiecutter.name}}/streamlit_app.py
+++ {{cookiecutter.name}}/streamlit_app.py
@@ -2,11 +2,5 @@ import streamlit as st
 
 
 st.set_page_config(page_title="{{cookiecutter.name}}", page_icon="random")
-_, center, _ = st.columns([2, 1, 2])
-with center:
-    st.image(
-        "{{cookiecutter.img}}",
-        use_column_width=True,
-    )
 st.title("{{cookiecutter.name}}")
 st.caption("{{cookiecutter.description}}")
